bf3caf0bf119139c35ea0a549db9dca944a4737e
SergeiKrivko <sergik.krivko@gmail.com>
2023-12-28 18:29:05 +0300

    fix keyboard

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import kivy
 from kivymd.uix.label import MDLabel
 
 try:
@@ -6,7 +5,7 @@ try:
     from kivymd.app import MDApp
 
     from kivy.core.window import Window
-    Window.softinput_mode = 'resize'
+    Window.softinput_mode = 'scale'
 
     from src import config
     from src.chat import ChatPanel
@@ -40,62 +39,7 @@ class MainApp(MDApp):
         return main_layout
 
 
-def update_viewport(self=Window):
-    from kivy.graphics.opengl import glViewport
-    from kivy.graphics.transformation import Matrix
-    from math import radians
-
-    w, h = self.system_size
-    if self._density != 1:
-        w, h = self.size
-
-    smode = self.softinput_mode
-    target = self._system_keyboard.target
-    targettop = max(0, target.to_window(0, target.y)[1]) if target else 0
-    kheight = self.keyboard_height
-
-    w2, h2 = w / 2., h / 2.
-    r = radians(self.rotation)
-
-    x, y = 0, 0
-    _h = h
-    if smode == 'pan':
-        y = kheight
-    elif smode == 'below_target':
-        y = 0 if kheight < targettop else (kheight - targettop)
-    if smode == 'scale':
-        _h -= kheight
-    if smode == 'resize':
-        y += kheight
-    # prepare the viewport
-    glViewport(x, y, w, _h)
-
-    # do projection matrix
-    projection_mat = Matrix()
-    projection_mat.view_clip(0.0, w, 0.0, h, -1.0, 1.0, 0)
-    self.render_context['projection_mat'] = projection_mat
-
-    # do modelview matrix
-    modelview_mat = Matrix().translate(w2, h2, 0)
-    modelview_mat = modelview_mat.multiply(Matrix().rotate(r, 0, 0, 1))
-
-    w, h = self.size
-    w2, h2 = w / 2., h / 2.
-    modelview_mat = modelview_mat.multiply(Matrix().translate(-w2, -h2, 0))
-    self.render_context['modelview_mat'] = modelview_mat
-    frag_modelview_mat = Matrix()
-    frag_modelview_mat.set(flat=modelview_mat.get())
-    self.render_context['frag_modelview_mat'] = frag_modelview_mat
-
-    # redraw canvas
-    self.canvas.ask_update()
-
-    # and update childs
-    self.update_childsize()
-
-
 if __name__ == "__main__":
     app = MainApp()
     app.run()
-    Window.update_viewport = update_viewport
     app.main_widget.db.close()
